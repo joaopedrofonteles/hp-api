9debf27157198187823f8348de9bdf4b3cec0625
joaopedrofonteles <dev.fonteles@gmail.com>
2024-06-06 22:22:59 -0300

    feat: add character type

---
+++ src/Types/character.ts
@@ -0,0 +1,10 @@
+export interface Character {
+  id: string; 
+  name: string;
+  gender: string; 
+  house: string;
+  wizard: boolean;
+  hogwartsStudent: boolean; 
+  image: string;
+  actor: string; 
+}
