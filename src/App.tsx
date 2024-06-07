e14600532b13e1490df6bd0fed864d9363a5496d
joaopedrofonteles <dev.fonteles@gmail.com>
2024-06-06 22:24:09 -0300

    feat: add get character from api and add initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import { Character } from "./Types/character";
+
 export default function App() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  const getCharactersFromApi = async () => {
+    const req = await fetch("https://hp-api.onrender.com/api/characters");
+    const data = (await req.json()) as Character[];
+    const slicedData = data.slice(0, 20);
+    setCharacters(slicedData);
+  };
+
+  getCharactersFromApi();
+
+  useEffect(() => {
+    getCharactersFromApi();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-zinc-900 px-4 pt-10">
+      <div className="mx-auto max-w-5xl px-10">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {characters.map((item) => (
+            <div
+              key={item.id}
+              className="overflow-hidden rounded-md bg-zinc-200"
+            >
+              <div className="flex gap-2">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="aspect-[3/4] h-[200px] object-cover"
+                />
+                <div>
+                  <strong className="text-lg">{item.name}</strong>
+                  <p>{item.gender === "male" ? "Masculino" : "Feminino"}</p>
+                  <p>{item.house}</p>
+                  <p>{item.hogwartsStudent ? "Estudante" : "Egresso"}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+// https://hp-api.onrender.com/api/characters
